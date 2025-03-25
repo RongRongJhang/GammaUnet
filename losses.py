f7b768635b3775f30874a43a5520926702704e1d
RongRongJhang <dandelionron@gmail.com>
2025-03-25 21:57:20 +0800

    "modify losses"

--- losses.py
+++ losses.py
@@ -17,6 +17,16 @@ class VGGPerceptualLoss(nn.Module):
         y_true, y_pred = y_true.to(next(self.loss_model.parameters()).device), y_pred.to(next(self.loss_model.parameters()).device)
         return F.mse_loss(self.loss_model(y_true), self.loss_model(y_pred))
 
+class L1CharbonnierLoss(torch.nn.Module):
+    def __init__(self):
+        super(L1CharbonnierLoss, self).__init__()
+        self.eps = 1e-6
+
+    def forward(self, X, Y):
+        diff = torch.add(X, -Y)
+        error = torch.sqrt(diff * diff + self.eps)
+        loss = torch.mean(error)
+        return loss
 
 def color_loss(y_true, y_pred):
     return torch.mean(torch.abs(torch.mean(y_true, dim=[1, 2, 3]) - torch.mean(y_pred, dim=[1, 2, 3])))
@@ -52,23 +62,27 @@ class CombinedLoss(nn.Module):
     def __init__(self, device):
         super(CombinedLoss, self).__init__()
         self.perceptual_loss_model = VGGPerceptualLoss(device)
+        self.charbonnier_loss_model = L1CharbonnierLoss(device)
         self.alpha1 = 1.00
         self.alpha2 = 0.06
         self.alpha3 = 0.05
         self.alpha4 = 0.5
         self.alpha5 = 0.0083
         self.alpha6 = 0.25
+        self.alpha7 = 0.1
 
     def forward(self, y_true, y_pred):
         smooth_l1_l = smooth_l1_loss(y_true, y_pred)
         ms_ssim_l = multiscale_ssim_loss(y_true, y_pred)
         perc_l = self.perceptual_loss_model(y_true, y_pred)
+        char_l = self.charbonnier_loss_model(y_true, y_pred)
         hist_l = histogram_loss(y_true, y_pred)
         psnr_l = psnr_loss(y_true, y_pred)
         color_l = color_loss(y_true, y_pred)
 
         total_loss = (self.alpha1 * smooth_l1_l + self.alpha2 * perc_l + 
                       self.alpha3 * hist_l + self.alpha5 * psnr_l + 
-                      self.alpha6 * color_l + self.alpha4 * ms_ssim_l)
+                      self.alpha6 * color_l + self.alpha4 * ms_ssim_l +
+                      self.alpha7 * char_l)
 
         return torch.mean(total_loss)
