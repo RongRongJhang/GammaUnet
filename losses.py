4992ddf84bcbf8b5390eec0afbdfee2b65ade9bd
RongRongJhang <dandelionron@gmail.com>
2025-03-25 23:34:04 +0800

    "modify losses"

--- losses.py
+++ losses.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 from pytorch_msssim import ms_ssim
+from pytorch_ssim import ssim
 import torchvision.transforms as T
 
 class VGGPerceptualLoss(nn.Module):
@@ -28,6 +29,9 @@ class L1CharbonnierLoss(torch.nn.Module):
         loss = torch.mean(error)
         return loss
 
+def mse_loss(pred, target):
+    return F.mse_loss(pred, target, reduction='none')
+
 def color_loss(y_true, y_pred):
     return torch.mean(torch.abs(torch.mean(y_true, dim=[1, 2, 3]) - torch.mean(y_pred, dim=[1, 2, 3])))
 
@@ -41,6 +45,9 @@ def smooth_l1_loss(y_true, y_pred):
 
 def multiscale_ssim_loss(y_true, y_pred, max_val=1.0, power_factors=[0.5, 0.5]):
     return 1.0 - ms_ssim(y_true, y_pred, data_range=max_val, size_average=True)
+
+def ssim_loss(y_true, y_pred):
+    return 1.0 - ssim(y_true, y_pred)
 
 def gaussian_kernel(x, mu, sigma):
     return torch.exp(-0.5 * ((x - mu) / sigma) ** 2)
@@ -75,14 +82,15 @@ class CombinedLoss(nn.Module):
         smooth_l1_l = smooth_l1_loss(y_true, y_pred)
         ms_ssim_l = multiscale_ssim_loss(y_true, y_pred)
         perc_l = self.perceptual_loss_model(y_true, y_pred)
-        char_l = self.charbonnier_loss_model(y_true, y_pred)
+        # char_l = self.charbonnier_loss_model(y_true, y_pred)
         hist_l = histogram_loss(y_true, y_pred)
         psnr_l = psnr_loss(y_true, y_pred)
         color_l = color_loss(y_true, y_pred)
+        mse_l = mse_loss(y_pred, y_true)
+        ssim_l = ssim_loss(y_true, y_pred)
 
-        total_loss = (self.alpha2 * perc_l + 
+        total_loss = (self.alpha1 * smooth_l1_l + self.alpha2 * perc_l + 
                       self.alpha3 * hist_l + self.alpha5 * psnr_l + 
-                      self.alpha6 * color_l + self.alpha4 * ms_ssim_l +
-                      self.alpha7 * char_l)
+                      self.alpha6 * color_l + self.alpha4 * ssim_l)
 
         return torch.mean(total_loss)
